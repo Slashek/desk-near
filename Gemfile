b1cc407e2932612d5c4db1df8b48e2b5e49a0e8b
Maciej Krajowski-Kukiel <mkk@quantads.net>
2012-12-20 01:37:12 +0100

    init

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'sqlite3'
+
+group :development, :test do
+    gem 'rspec-rails'
+    gem 'capybara'
+    gem 'spork-rails'
+    gem 'mysql2'
+    gem 'guard-rspec'
+    gem 'guard-spork'
+    gem 'factory_girl_rails'
+    gem 'rb-fsevent', '~> 0.9.1'
+    gem 'faker'
+    gem 'launchy'
+end
+
+group :assets do
+    gem 'sass-rails',   '~> 3.2.3'
+    gem 'coffee-rails', '~> 3.2.1'
+    gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesksNearMe::Application.config.secret_token = '07c5cf447cd8a41626c3db02684d702167c6273a28e803f1b3ed392661babd4dd78838d7b2df58da13631d5c1f0bf4529372efd6b52efdd661b1ed5aef791ea3'
